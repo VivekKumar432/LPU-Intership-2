316eaea2fd692a8e207081e67b5c21ae2c012fdf
Raghav Arora <raarora09@gmail.com>
2024-06-21 11:20:09 +0530

    added basic user and admin dashboard and added route security

--- basic-erp/frontend/src/App.js
+++ basic-erp/frontend/src/App.js
@@ -1,25 +1,34 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
-import './App.css';  // Ensure this import is here to apply the styles
+import UserDashboard from './pages/UserDashboard';
+import AdminDashboard from './pages/AdminDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   return (
-    <div className="app-container">
-      <Router>
-        <Navbar />
-        <div className="content-container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user-dashboard" element={
+          <ProtectedRoute roleRequired="user">
+            <UserDashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/admin-dashboard" element={
+          <ProtectedRoute roleRequired="admin">
+            <AdminDashboard />
+          </ProtectedRoute>
+        } />
+      </Routes>
+    </Router>
   );
 };
 
